c9edf7f603f21f7496fc96f0d4b72533d22bf1f7
Mem96 <ladyhinata96@gmail.com>
2022-06-16 20:02:39 +0200

    first step

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <h1>TIC TAC TOE</h1>
+    <div class='grid-container'>
+      <grid-component @winnerChosen='showWinnerMethod' ref='gridComponent'>
+      </grid-component>
+    </div>
+    <div class='showWinner' v-show='showWinner'>
+      <h2>{{winnerMessage}}</h2>
+      <button id='button' @click='resetGame'>Restart</button>
+    </div>
+        
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GridComponent from './components/GridComponent.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    GridComponent,
+  },
+  data () {
+    return {
+      winner: '',
+      winnerMessage: '',
+      showWinner: false,
+    }
+  },
+  methods: {
+    showWinnerMethod(player){
+      this.winner = player;
+      this.showWinner = true;
+      if (this.winner !== 'none'){
+        this.winnerMessage = `The winner is ${this.winner}`;
+      } else {
+        this.winnerMessage = `No one wins. Try again.`
+      }
+    },
+    resetGame(){
+      this.$refs.gridComponent.emptyBoard();
+      this.winner = '';
+      this.showWinner = false;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family: 'marker';
+    src: url("./assets/fonts/header-marker.otf");
+  }
+  @font-face {
+    font-family: 'thin-marker';
+    src: url("./assets/fonts/drawing-with-markers.ttf");
+  }
+  #app{
+    display: block;
+    height: 100vh;
+    /**/
+    background: url("./assets/squared-paper-texture.jpg");
+    background-size: cover;
+    font-family: 'marker';
+  }
+  h1{
+    margin: auto;
+    padding-top: 20px;
+    text-align: center;
+    font-family: 'marker';
+    font-weight: normal;
+    font-size: 80px;
+  }
+  .grid-container{
+    display: flex;
+    margin: auto;
+    margin-top: 20px;
+    align-items: center;
+    justify-content: center;
+  }
+  .showWinner{
+    text-align: center;
+    margin: auto;
+  }
+  .showWinner > h2{
+    font-family: 'thin-marker';
+    font-weight: bolder;
+    font-size: 25px;
+    margin-bottom: 40px;
+  }
+ .showWinner > button{
+    font-family: 'marker';
+    width: 300px;
+    font-size: 25px;
+    background-color: rgb(239, 189, 189);
+    border: solid 3px black;
+    border-radius: 15px;
+  }
+  .showWinner > button:hover{
+    background-color: rgb(244, 211, 221);
+  }
+
+  
 </style>
---
+++ src/components/CellComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div @click="$emit('clickOnCell')" class='cell' :style="{color: fontColor}">
+        <span>{{value}}</span>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        value: String,
+        fontColor: String,
+    }
+}
+</script>
+
+<style>
+    .cell{
+        width: 40px;
+        height: 40px;
+        margin: 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        user-select: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 30px;
+        border: 3px solid transparent
+    }
+    .cell:hover{
+        border: 3px dotted rgb(81, 139, 162);
+        border-radius: 50%;
+    }
+</style>
